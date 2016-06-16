b910132556e7cb18e50398d5356dd5b5effb485b
Yang <yang@truman.edu>
2016-06-16 15:34:57 -0400

    clean up search logic, introduce search css, and add tooltips for help information

--- public/js/search.js
+++ public/js/search.js
@@ -1,7 +1,8 @@
-// default search option values
-var search_options = {};
+// initialize search option object and store object
+var search_options = {}
+var store = {};
 
-// attach event listeners
+// attach event listeners and initialize tooltips
 $(document).ready(function(){
   // event listener for search options
   $( "#search-component" ).on( "click", 'input[name=scope]:checkbox, \
@@ -12,7 +13,77 @@ $(document).ready(function(){
 
   // event listener for query change
   $('#search-input').keyup(execute_search);
+
+  // add tooltips
+  $(".search-tooltips").tooltip({ placement: 'bottom'});
 })
+
+// create the index
+var index = elasticlunr(function(){
+    this.setRef('id'); // special unique identifier (id)
+    this.addField('type');
+    this.addField('title');
+    this.addField('url');
+    this.addField('page_url');
+    this.addField('image');
+    this.addField('content');
+    this.addField('preview');
+});
+
+// read json content from file using jQuery
+$.getJSON( "/search.json", function( data ) {
+  $.each( data, function( key, val ) {
+    index.addDoc(val);
+    store[val.id] = {
+      type: val.type,
+      title: val.title,
+      url: val.url,
+      page_url: val.page_url,
+      image: val.image,
+      content: val.content
+    };
+  });
+});
+
+// search logic
+var execute_search = function() {
+    // get search options
+    var expansion = $("input[name=expansion]:checked").val() == "expansion" ? true : false;
+    var boolean = $("input[name=boolean]:checked").val();
+    var fields = {};
+    $("input[name=scope]:checked").each(function () {
+        fields[this.value] = {};
+    });
+    search_options["expand"] = expansion;
+    search_options["fields"] = fields;
+    // search_options["boolean"] = boolean;
+
+    // get search query
+    var query = $('#search-input').val();
+
+    // empty result container when search query is empty
+    if (query === '') {
+        jQuery('.search-results').empty();
+    } else {
+        // perform search
+        var results = index.search(query, search_options);
+        $('#search-results').empty().append(
+            results.length ?
+            results.map( function (result) {
+                var href_url = store[result.ref].page_url || store[result.ref].url;
+                var el = $('<section class="search-result">')
+                    .append($('<a class="search-result-title">')
+                        .attr('href', href_url)
+                        .text(store[result.ref].title)
+                    );
+                if (store[result.ref].content) {
+                    el.after($('<p class="search-result-content">').text(store[result.ref].content));
+                }
+                return el;
+            }) : $('<section class="search-result"><p>No results found</p></section>')
+        );
+    }
+};
 
 // var page_template = '<div class="media {type}"> ' +
 //     '<h4><a href="{url}">{title}</a></h4>' +
@@ -32,75 +103,3 @@ $(document).ready(function(){
 //   searchResultTemplate: page_template,
 //   limit: 100,
 // });
-
-// create the index
-var index = elasticlunr(function(){
-    this.setRef('id'); // special unique identifier (id)
-    this.addField('type');
-    this.addField('title');
-    this.addField('url');
-    this.addField('page_url');
-    this.addField('image');
-    this.addField('content');
-    this.addField('preview');
-});
-
-// store information that will be retrieved later
-var store = {};
-
-// read json content from file using jQuery
-$.getJSON( "/search.json", function( data ) {
-  $.each( data, function( key, val ) {
-    index.addDoc(val);
-    store[val.id] = {
-      type: val.type,
-      title: val.title,
-      url: val.url,
-      page_url: val.page_url,
-      image: val.image,
-      content: val.content
-    };
-  });
-
-});
-
-// search logic
-var execute_search = function() {
-    // get search options
-    var expansion = $("input[name=expansion]:checked").val() == "expansion" ? true:false || true;
-    // var boolean = $("input[name=boolean]:checked").val() || "OR";
-    var fields = {};
-    $("input[name=scope]:checked").each(function () {
-        fields[this.value] = {};
-    });
-    search_options["expand"] = expansion;
-    search_options["fields"] = fields;
-    // search_options["boolean"] = boolean;
-
-    // get search query
-    var query = $('#search-input').val();
-
-    // empty result container when search query is empty
-    if (query === '') {
-        jQuery('.search-results').empty();
-    } else {
-        // perform search
-        var results = index.search(query, search_options);
-        debugger;
-        $('#search-results').empty().append(
-            results.length ?
-            results.map( function (result) {
-                var href_url = store[result.ref].page_url || store[result.ref].url;
-                var el = $('<section class="search-result">')
-                    .append($('<a class="search-result-title">')
-                        .attr('href', href_url)
-                        .text(store[result.ref].title)
-                    );
-                if (store[result.ref].content) {
-                    el.after($('<p class="search-result-content">').text(store[result.ref].content));
-                }
-                return el;
-            }) : $('<section class="search-result"><p>No results found</p></section>')
-        );
-    }
-};
